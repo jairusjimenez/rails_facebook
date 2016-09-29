2229872a0c3167393a42c812ce4e5bc7fc98e788
jairusjimenez <jairusjimenez@live.com>
2016-09-29 21:21:18 +0800

    Add image uploader to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,16 @@ class Post < ApplicationRecord
 	has_many :liking_users, through: :likes, source: :user
 	has_many :comments, dependent: :destroy
 	has_many :commenting_users, through: :comments, source: :user
+	mount_uploader :photo, PhotoUploader
+
+	validate :photo_size
+
+	private
+
+		def photo_size
+			if photo.size > 5.megabytes
+				errors.add(:photo, "should be smaller than 5mb")
+			end
+		end
 
 end
